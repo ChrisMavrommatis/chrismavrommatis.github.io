fe90dc992bb83aeaf1d98d6af7b173b6e12e6f8d
Chris Mavrommatis <56303004+ChrisMavrommatis@users.noreply.github.com>
2024-10-30 09:57:27 +0200

    some menu work and seo

--- src/_plugins/sanitization_filters.rb
+++ src/_plugins/sanitization_filters.rb
@@ -15,6 +15,26 @@ module Jekyll
 		  truncated = transformed.length > length ? "#{transformed[0, length]}..." : transformed
 		  truncated
 		end
+
+    def format_meta_description(input, length = 160)
+      input = input.to_s
+
+      transformed = input
+      .gsub('.', '. ')
+      .gsub('</h2>', ': ')
+      .gsub('</h3>', ': ')
+      .gsub('</h4>', ': ')
+      .gsub(/<\/?[^>]*>/, '') # Remove HTML tags
+      .gsub(/\n+/, ' ') # Remove new lines
+      .gsub(/ {2,}/, ' ') # Replace multiple spaces with a single space
+      .strip
+
+       # Truncate to the specified length
+       truncated = transformed[0...length]
+
+       # Return the final description
+       truncated
+    end
 	end
 end
 
